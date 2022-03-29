a2bbf4aae91c975a271b88b3301b1a718f6a5b11
Larryking007 <ajikobilanre007@gmail.com>
2022-03-29 03:15:11 +0100

    new todolist app

---
+++ todoapp/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css';
+import Form  from './components/Form';
+
+function App() {
+  return (
+    <div className="App">
+     <header>
+      <h1>Ed's Todo List</h1>
+    </header>
+    </div>
+  );
+}
+
+export default App;
