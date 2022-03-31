2de449dc0311c6aca107a9101bb06f5a72e5d4ef
Larryking007 <ajikobilanre007@gmail.com>
2022-03-31 13:11:25 +0100

    search bar

--- recipieapp/src/App.js
+++ recipieapp/src/App.js
@@ -1,21 +1,53 @@
+import React, { useEffect, useState } from "react";
+import Recipie from "./Recipie";
 import "./App.css";
 
 const App = () => {
   const APP_ID = "751b0dda";
   const APP_KEY = "9ca3306b4b4f8d712a546eec69bddfe2";
 
-  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+  const [recipies, setRecipies] = useState([]);
+  const [search, setSearch] = useState("");
 
+  useEffect(() => {
+    getRecipies();
+  }, []);
 
+  const getRecipies = async () => {
+    /* const response = await fetch(
+      `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+    );*/
+    const data = await response.json();
+    setRecipies(data.hits);
+    console.log(data.hits);
+  };
+
+  const updateSearch = (e) => {
+    setSearch(e.target.value);
+  };
   return (
     <div className="App">
-      <form className="search-form"> 
-        <input type="text">
-        
-        </input>
+      <form className="search-form">
+        <input
+          className="search-bar"
+          type="text"
+          value={search}
+          onChange={updateSearch}
+        />
+        <button className="search-button" type="submit">
+          Search
+        </button>
       </form>
+      {recipies.map((recipie) => (
+        <Recipie
+          key={recipie.recipie.label}
+          title={recipie.recipie.label}
+          calories={recipie.recipie.calories}
+          image={recipie.recipie.image}
+        />
+      ))}
     </div>
   );
-}
+};
 
 export default App;
